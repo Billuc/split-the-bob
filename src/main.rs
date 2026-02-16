9baf07972b86d4480d178748cecaf4f77e91fa60
Billuc <billuc@hotmail.fr>
2026-02-16 22:14:05 +0100

    Preparing for real work

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use axum::{ routing::{get, post}, Router, http::StatusCode, Json };
+use db::{DB};
 mod expense;
 mod expense_group;
 mod db;
 
-// #[tokio::main]
-fn main() {
-    // let app = Router::new()
-        // .route("/", get(async || { "Hello World" }));
+#[derive(Clone)]
+struct State {
+    db: DB,
+}
 
-    // let listener = tokio::net::TcpListener::bind("0.0.0.0:1221").await.unwrap();
-    // let _ = axum::serve(listener, app).await;
-    db::db();
+#[tokio::main]
+async fn main() {
+    let db = DB::new(".split-the-bob-data", "splits").unwrap();
+    let state = { db };
+
+    let app = Router::new()
+        .route("/", get(async || { "Hello World" }))
+        .with_state(state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:1221").await.unwrap();
+    let _ = axum::serve(listener, app).await;
 }
