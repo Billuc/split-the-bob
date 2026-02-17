24f352c427fcdf1b3409c1923e0b52ace051807d
Luc Billaud <billuc@hotmail.fr>
2026-02-17 10:46:40 +0100

    feat: added env vars for port and db_url

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use axum::{ routing::{get, post}, Router, http::StatusCode, Json };
 use db::{DB};
 use tower_http::services::ServeDir;
+use std::env;
 
 mod expense;
 mod expense_group;
@@ -13,16 +14,19 @@ struct State {
 
 #[tokio::main]
 async fn main() {
+    let port = env::var("PORT").unwrap_or_else(|_| "1221".to_string());
+    let db_url = env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite://stb.data".to_string());
+
     let static_files = ServeDir::new("./static");
-    let db = DB::new("sqlite://stb.data").await.unwrap();
+    let db = DB::new(&db_url).await.unwrap();
     let state = { db };
 
     let app = Router::new()
         .fallback_service(static_files)
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:1221").await.unwrap();
-    println!("Starting server on 0.0.0.0:1221");
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
+    println!("Starting server on 0.0.0.0:{port}");
     axum::serve(listener, app).await.unwrap();
 }
 
